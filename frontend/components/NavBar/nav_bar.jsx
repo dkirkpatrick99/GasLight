d9985e2a8474a3084bf9475eb322cc91eb59aa0d
CreatorDK <d.kirkpatrick99@gmail.com>
2020-05-01 21:39:10 -0700

    fixed navbar

--- frontend/components/NavBar/nav_bar.jsx
+++ frontend/components/NavBar/nav_bar.jsx
@@ -5,140 +5,140 @@ import {NavLink} from 'react-router-dom'
 
 class NavBar extends React.Component{
    
-    // constructor(props){
-    //     super(props)
-    //     this.state = {
-    //         username: '',
-    //         email: '',
-    //         password: ''
-    //     }
-    //     // this.myInput =  <div className="nav-dropdown">
-    //     //                     <div>
-    //     //                         hello
-    //     //                     </div>
-    //     //                     <div>sup</div>
-    //     //                     <div>buenos tardes</div>
-    //     //                 </div>
-    //     this.handleSubmit = this.handleSubmit.bind(this)
-    //     this.update = this.update.bind(this)
-    //     // this.toggleDrop = this.toggleDrop.bind(this)
+    constructor(props){
+        super(props)
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        }
+        // this.myInput =  <div className="nav-dropdown">
+        //                     <div>
+        //                         hello
+        //                     </div>
+        //                     <div>sup</div>
+        //                     <div>buenos tardes</div>
+        //                 </div>
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.update = this.update.bind(this)
+        // this.toggleDrop = this.toggleDrop.bind(this)
+    }
+
+    // componentDidMount() {
+    //     this.toggleDrop()
     // }
 
-    // // componentDidMount() {
-    // //     this.toggleDrop()
-    // // }
-
-    // update(field) {
-    //     return e => this.setState({
-    //       [field]: e.currentTarget.value
-    //     });
-    // }
+    update(field) {
+        return e => this.setState({
+          [field]: e.currentTarget.value
+        });
+    }
     
-    // handleSubmit(e) {
-    //     e.preventDefault();
-    //     const user = Object.assign({}, this.state);
-    //     this.props.processForm(user);
-    // }
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user);
+    }
     
     
-    // render() {
+    render() {
         
-    //     if (!this.props.currentUser) { 
-    //         return (
-    //             <div>
-    //         <header className="header">
-    //         <nav className="header-nav">
-    //             <h1 className="header-logo">
-    //             <ul className="left-header-items">
-    //                 <li className="logo">
-    //             <NavLink to={'/'}>MONEYWISE</NavLink>
-    //                 </li>
-    //             <li className="nav-button">
-    //                 <a href={'https://github.com/dkirkpatrick99/MoneyWise.git'}>Git Repo <i className="fas fa-chevron-down"></i></a>
-    //                 <ul className="drop-down">
-    //                 </ul>
-    //             </li>
-    //                 <li className="nav-button">
-    //             <a href={'https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/'}>Dalton's Portfolio</a>
-    //                 </li>
-    //                 {/* <li className="nav-button">
-    //                 <i class="fas fa-search"></i>
-    //                 </li> */}
-    //             </ul>
-    //             </h1>
-    //             <div className="nav-right">
-    //             <ul className="right-header-items">
-    //                 <li className="nav-button">
-    //                 <NavLink to={'/categories/0'}>Explore</NavLink>
-    //                 </li>
-    //                 <li className="nav-button">
-    //                 <a onClick={() => this.props.openModal('signup')}>Start a Campaign</a>
-    //                 </li>
-    //                 <li className="nav-button">
-    //                 <a onClick={() => this.props.openModal('login')}>Log In</a>
-    //                 </li>
-    //                 <li className="nav-button">
-    //                 <a onClick={() => this.props.openModal('signup')}>Sign Up</a>
-    //                 </li>
-    //             </ul>
-    //             </div>
-    //         </nav>
-    //         </header>
-    //     </div>
-    //         ) 
+        if (!this.props.currentUser) { 
+            return (
+                <div>
+            <header className="header">
+            <nav className="header-nav">
+                <h1 className="header-logo">
+                <ul className="left-header-items">
+                    <li className="logo">
+                <NavLink to={'/'}>MONEYWISE</NavLink>
+                    </li>
+                <li className="nav-button">
+                    <a href={'https://github.com/dkirkpatrick99/MoneyWise.git'}>Git Repo <i className="fas fa-chevron-down"></i></a>
+                    <ul className="drop-down">
+                    </ul>
+                </li>
+                    <li className="nav-button">
+                <a href={'https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/'}>Dalton's Portfolio</a>
+                    </li>
+                    {/* <li className="nav-button">
+                    <i class="fas fa-search"></i>
+                    </li> */}
+                </ul>
+                </h1>
+                <div className="nav-right">
+                <ul className="right-header-items">
+                    <li className="nav-button">
+                    <NavLink to={'/categories/0'}>Explore</NavLink>
+                    </li>
+                    <li className="nav-button">
+                    <a onClick={() => this.props.openModal('signup')}>Start a Campaign</a>
+                    </li>
+                    <li className="nav-button">
+                    <a onClick={() => this.props.openModal('login')}>Log In</a>
+                    </li>
+                    <li className="nav-button">
+                    <a onClick={() => this.props.openModal('signup')}>Sign Up</a>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+            </header>
+        </div>
+            ) 
             
-    //     } else {            
-    //         return (
-    //             <div>
-    //                 <header className="header">
-    //                     <nav className="header-nav">
-    //                         <h1 className="header-logo">
-    //                             <ul className="left-header-items">
-    //                                 <li className="logo">
-    //                                     <NavLink to={'/'}>MoneyWise</NavLink>
-    //                                 </li> 
-    //                                 <li className="nav-button drop-button">
-    //                                     <a href={'https://github.com/dkirkpatrick99/MoneyWise.git'}>Git Repo <i className="fas fa-chevron-down"></i></a>
-    //                                 </li>
-    //                                 <li className="nav-button">
-    //                                     <a href={'https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/'}>Dalton's Portfolio</a>
-    //                                 </li>
-    //                                 {/* <li className="nav-button">
-    //                                     <i className="fas fa-search"></i>
-    //                                 </li> */}
-    //                             </ul>
-    //                         </h1>
-    //                         <div className="nav-right">
-    //                             <ul className="right-header-items">
-    //                                 <li className="nav-button">
-    //                                     <NavLink to={'/categories/0'}>Explore</NavLink>
-    //                                 </li>
-    //                                 <li className="nav-button">
-    //                                     <NavLink to={"/campaigns/new"} href="#">Start a Campaign</NavLink>
-    //                                 </li>
-    //                                 <li className="nav-button">
-    //                                     <a onClick={this.props.logout}>Sign Out</a>
-    //                                 </li>
-    //                                 <li className="nav-button current-user">
-    //                                     <NavLink to={`/users/${this.props.currentUser.id}`}><i className="fas fa-robot"></i> &nbsp; {this.props.currentUser.username}
-    //                                     </NavLink>                                 
-    //                                 </li>
-    //                             </ul>
-    //                     </div>
-    //                 </nav>
-    //             </header>
+        } else {            
+            return (
+                <div>
+                    <header className="header">
+                        <nav className="header-nav">
+                            <h1 className="header-logo">
+                                <ul className="left-header-items">
+                                    <li className="logo">
+                                        <NavLink to={'/'}>MoneyWise</NavLink>
+                                    </li> 
+                                    <li className="nav-button drop-button">
+                                        <a href={'https://github.com/dkirkpatrick99/MoneyWise.git'}>Git Repo <i className="fas fa-chevron-down"></i></a>
+                                    </li>
+                                    <li className="nav-button">
+                                        <a href={'https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/'}>Dalton's Portfolio</a>
+                                    </li>
+                                    {/* <li className="nav-button">
+                                        <i className="fas fa-search"></i>
+                                    </li> */}
+                                </ul>
+                            </h1>
+                            <div className="nav-right">
+                                <ul className="right-header-items">
+                                    <li className="nav-button">
+                                        <NavLink to={'/categories/0'}>Explore</NavLink>
+                                    </li>
+                                    <li className="nav-button">
+                                        <NavLink to={"/campaigns/new"} href="#">Start a Campaign</NavLink>
+                                    </li>
+                                    <li className="nav-button">
+                                        <a onClick={this.props.logout}>Sign Out</a>
+                                    </li>
+                                    <li className="nav-button current-user">
+                                        <NavLink to={`/users/${this.props.currentUser.id}`}><i className="fas fa-robot"></i> &nbsp; {this.props.currentUser.username}
+                                        </NavLink>                                 
+                                    </li>
+                                </ul>
+                        </div>
+                    </nav>
+                </header>
 
-    //             {/* <div className="nav-dropdown">
-    //                 <div>
-    //                     hello
-    //                 </div>
-    //                 <div>sup</div>
-    //                 <div>buenos tardes</div>
-    //             </div> */}
-    //         </div>
-    //         )
-    //     }
-    // }
+                {/* <div className="nav-dropdown">
+                    <div>
+                        hello
+                    </div>
+                    <div>sup</div>
+                    <div>buenos tardes</div>
+                </div> */}
+            </div>
+            )
+        }
+    }
     
     
     // toggleDrop() {
